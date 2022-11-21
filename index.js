d284c579f3f373bb6eb2194d8ebaf9f42d2189e4
sibghataziz <sibghat.aziz98@gmail.com>
2022-11-22 00:53:43 +0530

    kuch bhi

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
+app.get("/",(req,res)=>{
+  return res.send("<h1>Welcone to Hostel Management</h1>")
+})
 app.use('/auth', userRouter);
 app.use(checkAuth)
 app.use('/hostel', hostelRouter);
